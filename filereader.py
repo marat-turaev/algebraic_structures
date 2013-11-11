f522ed34dedc6e6194a2d4733d3722556b22868e
Bogdan Bugaev <bogdan.bugaev@gmail.com>
2013-11-11 19:06:47 +0400

    Добавлен скрипт чтения слов из файла.

---
+++ filereader.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# coding: utf8
+
+
+def read_file(path):
+    """
+    Возвращает список слов из файла, находящегося по заданному пути path.
+    """
+    with open(path, 'r') as src:
+        text = src.read().strip()
+    return text.split()
+
+
+def read_grf():
+    """
+    Возвращает список слов из файла 'texts/grf_processed_2.txt',
+    содержащего обработанные задачи с замененными формулами.
+    """
+    return read_file('texts/grf_processed_2.txt')

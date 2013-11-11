b4eda2a55984836209b3395b9fc87185de153c52
Bogdan Bugaev <bogdan.bugaev@gmail.com>
2013-11-11 19:31:38 +0400

    Исправлен баг со склеиванием формул.

--- texts/process.py
+++ texts/process.py
@@ -11,14 +11,14 @@ def main():
     raw = re.sub('-[\n\r]{1,2}', '', raw)
     raw = re.sub('([,:;\.\?])', ' \g<1> ', raw)
     raw = re.sub('\s+', ' ', raw)
-    #raw = re.sub('\(cid:.*?\)', '', raw)
     raw = re.sub('\(.*?\)', '', raw)
 
     tokens = ['$f$' if re.match('(?i)[^\?\.,:;а-яё]|[·ϕα\d]', t) else t
               for t in raw.split()]
     text = ' '.join(tokens)
     text = re.sub('[,:; \.\?]{4,}', '$f$', text)
-    text = re.sub('\$f\$(?:[,:; \.\?]|(?:\$f\$))+\$f\$', '$f$', text)
+    text = re.sub('\$f\$(?:[,:; \.\?]|(?:\$f\$))*\$f\$', ' $f$ ', text)
+    text = re.sub('\s+', ' ', text)
 
     print text
 

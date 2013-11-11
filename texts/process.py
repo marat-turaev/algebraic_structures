e01466f3b307abbd0293f7b1cdcc327b86118c22
Bogdan Bugaev <bogdan.bugaev@gmail.com>
2013-11-11 18:33:35 +0400

    Допилен скрипт обработки текста задач. Добавлен файл с замененными на $f$ формулами.

--- texts/process.py
+++ texts/process.py
@@ -1,18 +1,25 @@
 #! /usr/bin/env python
-#coding: utf8
+# coding: utf8
 
 import re
 import sys
 
 
 def main():
-    text = sys.stdin.read().strip()
-    text = re.sub('(?m)^.?\d+[\.\)]? ?', '', text)
-    text = re.sub('-[\n\r]{1,2}', '', text)
-    text = re.sub('\s+', ' ', text)
-    text = re.sub('\(cid:.*?\)', '', text)
-    text = re.sub('\((?:ср|см).*?\)', '', text)
-    #text = re.sub('\w', '$formula$', text)
+    raw = sys.stdin.read().strip()
+    raw = re.sub('(?m)^.?\d+[\.\)]? ?', '', raw)
+    raw = re.sub('-[\n\r]{1,2}', '', raw)
+    raw = re.sub('([,:;\.])', ' \g<1> ', raw)
+    raw = re.sub('\s+', ' ', raw)
+    #raw = re.sub('\(cid:.*?\)', '', raw)
+    raw = re.sub('\(.*?\)', '', raw)
+
+    tokens = ['$f$' if re.match('(?i)[^\.,:;а-яё]|[·ϕα\d]', t) else t
+              for t in raw.split()]
+    text = ' '.join(tokens)
+    text = re.sub('[,:; \.]{4,}', '$f$', text)
+    text = re.sub('\$f\$(?:[,:; \.]|(?:\$f\$))+\$f\$', '$f$', text)
+
     print text
 
 

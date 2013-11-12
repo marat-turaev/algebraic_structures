15059ae54cf4a2a01b461d53f968f21654a9b84e
Bogdan Bugaev <bogdan.bugaev@gmail.com>
2013-11-12 21:52:05 +0400

    Логика работы веб-странички доведена до более-менее рабочего состояния. Добавлен шаблон страницы ошибки скачивания.

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    task = markov.generate(2).decode('utf8')
+    task = markov.generate(3).decode('utf8')
     return flask.render_template('index.html', task=task)
 
 
@@ -18,9 +18,21 @@ def download():
     if flask.request.method != 'POST':
         return flask.redirect(flask.url_for('index'))
 
-    tex = 'text'
+    number = flask.request.form['number']
+    if not number.isdigit():
+        return flask.render_template('error.html',
+                                     msg=u'Задано некорректное число задач.')
+    else:
+        number = int(number)
+        if number <= 0:
+            return flask.render_template('error.html',
+                                         msg=u'Задано слишком маленькое число задач.')
+        if number > 100:
+            return flask.render_template('error.html',
+                                         msg=u'Задано слишком большое число задач.')
+
+    tex = markov.generate_tex(number)
     response = flask.make_response(tex)
-    #response.headers['Content-Type'] = 'application/pdf'
     response.headers['Content-Type'] = 'application/x-tex'
     response.headers['Content-Disposition'] = 'attachment; filename="tasks"'
     return response
